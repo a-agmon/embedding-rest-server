95a0c4b306ae680cdfd50f20c3fb89e02c130350
Alon Agmon <alon.agmon@gmail.com>
2022-06-17 13:53:55 +0300

    adding perf tests files

--- load_test.js
+++ load_test.js
@@ -1,13 +1,19 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  vus: 500,
+  duration: '5s',
+};
 export default function () {
-  http.get('https://localhost:8080/mostsimilar');
+  http.get('http://127.0.0.1:8080/similar?to=Eminem&topk=10');
   sleep(1);
 }
 
-//ab -n 10 -c 2 http://127.0.0.1:8080/similar?to=Eminem&topk=10
 
+// this was run using k6 run load_test.js
+
+//ab -n 10 -c 2 http://127.0.0.1:8080/similar?to=Eminem&topk=10
 //ab -p test_payload.txt -T application/json  -c 10 -n 10 http://127.0.0.1:8080/mostsimilar
 
 
